e6dc52d02c4e8206379b13ed507dc962495c83ae
HENRIQUE1223344 <130380853+HENRIQUE1223344@users.noreply.github.com>
2023-09-04 11:47:23 -0300

    Add files via upload

---
+++ introducao-javascript/principal.js
@@ -0,0 +1,9 @@
+alert ("o arquivo separao está funcionando")
+
+
+//busc no sitepelo titulo e armazena ele dentro da varieavel
+let titulo = document.querySelector("titulo"); 
+//mostra no devtools o conteudo de texto desejado
+console.log(titulo.textContent);
+titulo.textContent =  "Fica Grande Nutrição"
+
